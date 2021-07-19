599bbbc596b8b68187ddda9165866746f00a9414
Knarik_Dabaghyan <kdabaghyan@zscaler.com>
2021-07-19 17:33:37 +0400

    update GmailTest.java

--- src/test/java/project/utils/TestListener.java
+++ src/test/java/project/utils/TestListener.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import project.driver.DriverSingleton;
@@ -19,32 +18,8 @@ import java.time.format.DateTimeFormatter;
 public class TestListener implements ITestListener {
     protected Logger log = LogManager.getRootLogger();
 
-    public void onTestStart(ITestResult iTestResult) {
-
-    }
-
-    public void onTestSuccess(ITestResult iTestResult) {
-
-    }
-
     public void onTestFailure(ITestResult iTestResult) {
         saveScreenshot();
-    }
-
-    public void onTestSkipped(ITestResult iTestResult) {
-
-    }
-
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
-    }
-
-    public void onStart(ITestContext iTestContext) {
-
-    }
-
-    public void onFinish(ITestContext iTestContext) {
-
     }
 
     private void saveScreenshot(){
@@ -56,8 +31,9 @@ public class TestListener implements ITestListener {
                     ".//target/screenshots/"
                     + getCurrentTimeAsString() +
                     ".png"));
+            log.error("screenshot saved");
         } catch (IOException e) {
-            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+            log.error("Failed to save screenshot");
         }
     }
 
